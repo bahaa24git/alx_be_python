6e72066777630879a91f899070c1690dfd412c38
bahaa24git <ahmad12.alnaggar34@gmail.com>
2025-07-14 00:10:14 +0300

    main

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000 # Principal amount
+rate = 0.05      # Interest rate in percent
+time = 3        # Time in years
+interst = (principal * rate * time) / 100  # Simple interest formula
+
+print(f"The simple interest is: {interst}")
