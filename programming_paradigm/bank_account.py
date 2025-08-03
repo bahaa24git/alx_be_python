1a4cfe6cc17f65c09e2997c9181222655adeee39
bahaa24git <ahmad12.alnaggar34@gmail.com>
2025-08-03 19:17:24 +0300

    fixed errors for auto checker

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,16 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
 
     def deposit(self, amount):
         if amount > 0:
-            self.account_balance += amount
-            return f"Deposited {amount}. New balance is {self.account_balance}."
-        else:
-            return "Deposit amount must be positive."
+            self.__account_balance += amount
 
     def withdraw(self, amount):
-        if amount > 0 and amount <= self.account_balance:
-            self.account_balance -= amount
+        if amount > 0 and self.__account_balance >= amount:
+            self.__account_balance -= amount
             return True
-        elif amount > self.account_balance:
-            return False
+        return False
 
     def display_balance(self):
-        return f"Account balance is {self.account_balance}."
+        return f"Current Balance: ${self.__account_balance}"
